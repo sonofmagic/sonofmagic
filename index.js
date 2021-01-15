d06707443a1e1ac68cbdcfcb933e7f4c8baabd2a
icebreaker99 <qq1324318532@gmail.com>
2021-01-15 16:05:05 +0800

    feat: commit for ts

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const path = require('path')
 const dayjs = require('dayjs')
 const Font = require('ascii-art-font')
 
-;(async () => {
-  const today = dayjs().format('YYYY-MM-DD')
+;
+(async () => {
+  const now = dayjs()
+  const today = now.format('YYYY-MM-DD')
+  const moment = now.format('YYYY-MM-DD HH:mm:ss')
   /**
    * @type {String} output
    */
@@ -18,6 +21,6 @@ const Font = require('ascii-art-font')
   ])
   // markdown 一行后面加2个空格后换行是小换行，直接换行是大换行
   // .split('\n').join('  \n')
-  const result = template.replace(/{{replace}}/g, (o1 + o2).trimEnd())
+  const result = template.replace(/{{replace}}/g, (o1 + o2).trimEnd()).replace(/{{date}}/g, moment)
   await fsp.writeFile(path.resolve(__dirname, 'README.md'), result)
 })()

8ade26fb0fd95aad0cd93f52b0c3eec305e7294c
SonOfMagic <qq1324318532@gmail.com>
2021-07-15 01:14:57 +0800

    feat: add ascii color

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+// require('dotenv').config()
 const fsp = require('fs').promises
 const path = require('path')
 const dayjs = require('dayjs')
 const Font = require('ascii-art-font')
+const ansi = require('ascii-art-ansi')
 
-;
-(async () => {
+;(async () => {
   const now = dayjs()
   const today = now.format('YYYY-MM-DD')
   const moment = now.format('YYYY-MM-DD HH:mm:ss')
@@ -21,6 +21,15 @@ const Font = require('ascii-art-font')
   ])
   // markdown 一行后面加2个空格后换行是小换行，直接换行是大换行
   // .split('\n').join('  \n')
-  const result = template.replace(/{{replace}}/g, (o1 + o2).trimEnd()).replace(/{{date}}/g, moment)
+  const matrix = template
+    .replace(/{{replace}}/g, (o1 + o2).trimEnd())
+    .replace(/{{date}}/g, moment)
+
+  const result = ansi.map(matrix, (chr, codes, rowcol, pos, shortcircuit) => {
+    // console.log(chr, codes, rowcol, pos, shortcircuit)
+    const char = ansi.Codes(chr, 'yellow')
+    return char
+  })
+
   await fsp.writeFile(path.resolve(__dirname, 'README.md'), result)
 })()

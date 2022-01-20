32006b7409eb42ef438ab37192fb1ada269ab063
SonOfMagic <qq1324318532@gmail.com>
2022-01-20 22:39:14 +0800

    feat: commit a svg

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const fsp = require('fs').promises
 const path = require('path')
 const dayjs = require('dayjs')
 const Font = require('ascii-art-font')
-// const ansi = require('ascii-art-ansi')
 
-;(async () => {
+  // const ansi = require('ascii-art-ansi')
+
+  ; (async () => {
   const now = dayjs()
   const today = now.format('YYYY-MM-DD')
   const moment = now.format('YYYY-MM-DD HH:mm:ss')
   /**
-   * @type {String} output
-   */
+       * @type {String} output
+       */
   const [o1, o2, template] = await Promise.all([
     Font.create('UTC :', 'Doom'),
     Font.create(today, 'Doom'),
@@ -19,11 +20,14 @@ const Font = require('ascii-art-font')
       encoding: 'utf-8'
     })
   ])
+
+  const icebreakerSvg = await fsp.readFile('./icebreaker.svg', 'utf-8')
   // markdown 一行后面加2个空格后换行是小换行，直接换行是大换行
   // .split('\n').join('  \n')
   const matrix = template
     .replace(/{{replace}}/g, (o1 + o2).trimEnd())
     .replace(/{{date}}/g, moment)
+    .replace(/{{icebreakerSvg}}/g, icebreakerSvg)
 
   // const result = ansi.map(matrix, (chr, codes, rowcol, pos, shortcircuit) => {
   //   // console.log(chr, codes, rowcol, pos, shortcircuit)

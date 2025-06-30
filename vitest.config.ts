da88773eafe97107332221b0e0bc6eacf4a87377
ice breaker <1324318532@qq.com>
2025-06-30 22:34:06 +0800

    chore: ci update

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig(
+  () => {
+    return {
+      test: {
+        projects: [
+          'packages/*',
+          'apps/*',
+        ],
+        coverage: {
+          enabled: true,
+          all: false,
+          skipFull: true,
+        },
+        forceRerunTriggers: [
+          '**/{vitest,vite}.config.*/**',
+        ],
+      },
+    }
+  },
+)
